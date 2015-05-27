7d5b412adde48b3f7b4b6d08f9a86b06888bfe1a
Ziver Koc <ziver@koc.se>
2015-05-27 13:13:19 +0000

    Updated MIT licence

--- src/zutil/parser/wsdl/WSDLHttpPage.java
+++ src/zutil/parser/wsdl/WSDLHttpPage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Ziver
+ * Copyright (c) 2015 ezivkoc
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
