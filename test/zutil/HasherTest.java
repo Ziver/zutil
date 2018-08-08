8b4217c1d39d9c48d827bf110bf4277dc4a71f55
Ziver Koc <ziver@koc.se>
2018-08-08 19:53:59 +0200

    Added test for PBKDF2

--- test/zutil/HasherTest.java
+++ test/zutil/HasherTest.java
@@ -33,19 +33,29 @@ public class HasherTest {
 
     @Test
     public void MD5Test(){
-        assertEquals(Hasher.MD5("AAAABBBB"), 			"9da4fc50e09e5eeb8ae8149ef4f23792");
-        assertEquals(Hasher.MD5("qwerty12345"), 		"85064efb60a9601805dcea56ec5402f7");
-        assertEquals(Hasher.MD5("123456789"), 			"25f9e794323b453885f5181f1b624d0b");
-        //assertEquals(Hasher.MD5(".,<>|!#�%&/()=?"), 	"20d5cda029514fa49a8bbe854a539847");
-        assertEquals(Hasher.MD5("Test45"), 				"fee43a4c9d88769e14ec6a1d8b80f2e7");
+        assertEquals(Hasher.MD5("AAAABBBB"),   "9da4fc50e09e5eeb8ae8149ef4f23792");
+        assertEquals(Hasher.MD5("qwerty12345"),"85064efb60a9601805dcea56ec5402f7");
+        assertEquals(Hasher.MD5("123456789"),  "25f9e794323b453885f5181f1b624d0b");
+        assertEquals(Hasher.MD5("Test45"),     "fee43a4c9d88769e14ec6a1d8b80f2e7");
     }
 
     @Test
     public void SHA1Test(){
-        assertEquals(Hasher.SHA1("AAAABBBB"), 			"7cd188ef3a9ea7fa0ee9c62c168709695460f5c0");
-        assertEquals(Hasher.SHA1("qwerty12345"), 		"4e17a448e043206801b95de317e07c839770c8b8");
-        assertEquals(Hasher.SHA1("123456789"), 			"f7c3bc1d808e04732adf679965ccc34ca7ae3441");
-        //assertEquals(Hasher.SHA1(".,<>|!#�%&/()=?"), 	"6b3de029cdb367bb365d5154a197294ee590a77a");
-        assertEquals(Hasher.SHA1("Test45"), 			"9194c6e64a6801e24e63a924d5843a46428d2b3a");
+        assertEquals(Hasher.SHA1("AAAABBBB"),   "7cd188ef3a9ea7fa0ee9c62c168709695460f5c0");
+        assertEquals(Hasher.SHA1("qwerty12345"),"4e17a448e043206801b95de317e07c839770c8b8");
+        assertEquals(Hasher.SHA1("123456789"),  "f7c3bc1d808e04732adf679965ccc34ca7ae3441");
+        assertEquals(Hasher.SHA1("Test45"),     "9194c6e64a6801e24e63a924d5843a46428d2b3a");
+    }
+
+    @Test
+    public void PBKDF2(){
+        assertEquals(Hasher.PBKDF2("AAAABBBB", "s", 1000),
+                "8da1853fe2a2efc82aa444e7274b74cf25190e580898bbaac83c502099506dd7");
+        assertEquals(Hasher.PBKDF2("AAAABBBB", "salt", 1000),
+                "550057b797d2e862c5a9aea9b9fae7c47de4f8179b6c6348f75cc2ede8322329");
+        assertEquals(Hasher.PBKDF2("password", "salt", 500),
+                "5078bf72f89ba6f1b69cc1697eccde1df5758a157707cb1bad0ef459dcd2eec3");
+        assertEquals(Hasher.PBKDF2("password", "salt", 1000),
+                "6e88be8bad7eae9d9e10aa061224034fed48d03fcbad968b56006784539d5214");
     }
 }
